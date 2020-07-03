5fe028b95ab35b416f6c183699c383007e86aaf8
Ben Chan <ben@chanhq.com>
2020-07-03 19:34:01 +0100

    Variables example part 1

--- variables.js
+++ variables.js
@@ -1,1 +1,21 @@
+// Types of Values
+// Number 3, 3.4
+// "String"
+// Boolean true and false
+// Undefined
+// Null
+// Symbol (ECMAScript 6)
+// Object
+
 // Variables
+var message = "Hello World";
+var age = 25;
+var hasCar = true;
+
+// Comparisions
+// !==
+// ===
+// >=
+// <=
+// >
+// <
