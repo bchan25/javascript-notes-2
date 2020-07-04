cf190c1eec9593ba00ae9ed716a9b7b9d81424ae
Ben Chan <ben@chanhq.com>
2020-07-04 10:54:38 +0100

    Added data structures

--- datastructures.js
+++ datastructures.js
@@ -16,3 +16,39 @@ list.pop(); // Remove last element
 list.push("elephant"); // Add element at end
 var newList = list.concat(["bee", "bear"]); // Create a new array
 list.sort(); // Sort original list
+
+// Objects
+var user = {
+  name: "Ben",
+  age: 32,
+  hobby: "Football",
+  hasCar: true,
+  toDo: ["Walk dog", "Buy food", "Wash car"],
+  shout: function () {
+    console.log("SHOUTING!!!!");
+  },
+};
+
+// Add
+user.favouriteFood = "Curry";
+
+// Change Value
+user.hobby = "Racing";
+
+// Call method
+user.shout();
+
+// Array of Objects
+var userList = [
+  {
+    username: "ben",
+    password: "1234",
+  },
+  {
+    username: "sarah",
+    password: "password",
+  },
+];
+
+// Access sarah password
+userList[1].password;
